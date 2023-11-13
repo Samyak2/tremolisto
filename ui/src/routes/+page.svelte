47c64a965bbe3497f1f9a9a1eec316756f963e82
Samyak S Sarnayak <samyak201@gmail.com>
2023-11-13 20:40:51 +0530

    responsive table

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -111,40 +111,125 @@
 		<colgroup>
 			<col />
 			<col />
-			<col class="w-40" />
-			<col class="w-20" />
+			<col class="sm:w-40" />
+			<col class="sm:w-20" />
 			<col />
 		</colgroup>
-		<tr>
-			<th class="border-primary-700 border-y-2">Song</th>
-			<th class="border-primary-700 border-y-2 border-r-2">Artist</th>
-			<th class="border-primary-700 border-y-2">Type</th>
-			<th class="border-primary-700 border-y-2">Length</th>
-			<th class="border-primary-700 border-y-2">Audio</th>
-		</tr>
-		{#each musics as music (music.key)}
+		<thead>
 			<tr>
-				<td rowspan={music.numParts() + 1} class="border-primary-700 text-center"
-					><h2 class="py-2 px-4 text-lg">
-						{music.title}
-					</h2></td
-				>
-				<td rowspan={music.numParts() + 1} class="border-primary-700 border-r-2 text-center"
-					><h2 class="py-2 px-4 text-lg">
-						{music.artist}
-					</h2></td
-				>
+				<th class="border-primary-700 border-y-2">Song</th>
+				<th class="border-primary-700 border-y-2 border-r-2">Artist</th>
+				<th class="border-primary-700 border-y-2">Type</th>
+				<th class="border-primary-700 border-y-2">Length</th>
+				<th class="border-primary-700 border-y-2">Audio</th>
 			</tr>
-
-			{#each music.getParts() as part, index ((music.id, part.id))}
-				<tr class="border-primary-700" class:border-b-2={index === music.numParts() - 1}>
-					<td class="border-primary-700 border-b-2 text-center">{part.typ} {part.extra}</td>
-					<td class="border-primary-700 border-b-2 text-center">{part.lengthPretty()}</td>
-					<td class="border-primary-700 border-b-2"
-						><audio controls preload="none" src="/{part.filename}" /></td
+		</thead>
+		<tbody>
+			{#each musics as music (music.key)}
+				<tr>
+					<td data-th="" rowspan={music.numParts() + 1} class="border-primary-700 text-center"
+						><span class="py-2 pl-4 md:px-4 text-xl font-bold">
+							{music.title}
+						</span></td
+					>
+					<td
+						data-hyphen-before=""
+						data-th=""
+						rowspan={music.numParts() + 1}
+						class="border-primary-700 md:border-r-2 text-center"
+						><span class="py-2 pl-2 md:px-4 text-xl font-bold">
+							{music.artist}
+						</span></td
 					>
 				</tr>
+
+				{#each music.getParts() as part, index ((music.id, part.id))}
+					<tr
+						class="border-primary-700 pl-2 md:pl-0 mb-2 md:mb-0"
+						class:border-b-2={index === music.numParts() - 1}
+					>
+						<td data-th="" class="border-primary-700 md:border-b-2 text-center"
+							>{part.typ} {part.extra}</td
+						>
+						<td
+							data-hyphen-before=""
+							data-th=""
+							class="border-primary-700 md:border-b-2 text-center"
+							><span class="pl-1 md:pl-0">{part.lengthPretty()}</span></td
+						>
+						<br class="block md:hidden" />
+						<td data-th="" class="border-primary-700 md:border-b-2"
+							><audio controls preload="none" src="/{part.filename}" /></td
+						>
+					</tr>
+				{/each}
 			{/each}
-		{/each}
+		</tbody>
 	</table>
 </div>
+
+<style>
+	/* ref: https://css-tricks.com/responsive-data-tables/ */
+	/* 
+Max width before this PARTICULAR table gets nasty
+This query will take effect for any screen smaller than 760px
+and also iPads specifically.
+*/
+	@media only screen and (max-width: 768px),
+		(min-device-width: 768px) and (max-device-width: 1024px) {
+		/* Force table to not be like tables anymore */
+		table,
+		thead,
+		tbody,
+		th,
+		td,
+		tr {
+			display: block;
+		}
+
+		/* Hide table headers (but not display: none;, for accessibility) */
+		thead tr {
+			position: absolute;
+			top: -9999px;
+			left: -9999px;
+		}
+
+		tr {
+			/* border: 1px solid #ccc; */
+			text-align: center;
+		}
+
+		td {
+			/* Behave  like a "row" */
+			/* border: none; */
+			/* border-bottom: 1px solid #eee; */
+			/*
+			position: relative;
+			padding-left: 50%;
+      */
+			display: inline-flex;
+			align-items: center;
+		}
+
+		td:before {
+			/* Now like a table header */
+			/* position: absolute; */
+			/* Top/left values mimic padding */
+			/*
+			top: 6px;
+			left: 6px;
+			width: 45%;
+			padding-right: 10px;
+			white-space: nowrap;
+      */
+		}
+
+		td[data-th]:before {
+			content: attr(data-th);
+		}
+
+		td[data-hyphen-before]:before {
+			content: '-';
+		}
+	}
+</style>
